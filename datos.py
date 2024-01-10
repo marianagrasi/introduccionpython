e97404e19a109df496c56d3a85237b75dbea77a0
marianagrasi <marianagranados201@gmail.com>
2024-01-09 20:38:33 -0500

    nt1 se implementa algoritmo de datos primitivos y suma desde consola

---
+++ datos.py
@@ -0,0 +1,4 @@
+numeroUno=int(input("Digita el primer numero entero: "))
+numeroDos=int(input("Digita el segundo numero entero: "))
+sumatoria=numeroUno+numeroDos
+print(f"La suma de {numeroUno} + {numeroDos} = {sumatoria}")
---
+++ main.py
@@ -0,0 +1,21 @@
+#Variables primitivas con python
+#Datos primitivos= datos bases para resolver un problema lo son los numeros, los boolean
+#los caracteres
+#Siempre se declara y luego se asigana la variablre
+#Asignar
+#nombreEstudiante=None
+#QUEMAR VARIABLES=YO MISMO ASIGNARLE VALOR
+nombreEstudiante="Mariana Granados Sierra"
+edadEstudiante=22
+#Para los decimales simepre se dsepara con . la coma solp para separar
+estaturaEstudiante=1.60
+hinchaDelMejor=True
+
+#PEDIR DATOS POR TECLADO
+comidaFavorita=input("Digita tu comida favorita: ")
+
+#MOSTRAR DATOS EN PANTALLA (SALIDAS)
+print(f"Hola {nombreEstudiante} tu comida favorita es {comidaFavorita}")
+
+
+
